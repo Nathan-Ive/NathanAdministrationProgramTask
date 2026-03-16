f1821920a1fa4673d9ad2f207980d2f2eafec4c1
Nathan Ivens <2242205@talnet.nl>
2026-03-16 10:56:28 +0100

    Created UserEdit class and added summaries of class functions
    I added explanations for what the original User Class is intended to do, and I created the UserEdit class to serve as the first step in creating the AddUser function.

--- AdministrationProgramNathan/User.cs
+++ AdministrationProgramNathan/User.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace AdministrationProgramNathan
 {
+
+    //This class will contain all Customer(User) Information
+    //Intended to be used with: View users, Searching user information, Reading saved customer information from a JSON file
+    //To summarize, this Class will specialize in reading and presenting user information that is created in the UserEdit Class.
+    //It will also be able to read and present data from an existing JSON file on startup that contains the relevant information for the program.
     internal class User
     {
     }
---
+++ AdministrationProgramNathan/UserEdit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministrationProgramNathan
+{
+
+    //This class exists to edit information within the User Class
+    //This class will be intended to: Add users, Remove users, Adjust/Edit user information
+    //To summarize, this Class will specialize in writing and sending user information to the User class.
+    internal class UserEdit
+    {
+    }
+}
