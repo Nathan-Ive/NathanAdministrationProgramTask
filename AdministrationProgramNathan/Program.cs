6c0603274086582aed542444eb2f466bbbe21a3d
Nathan Ivens <2242205@talnet.nl>
2026-03-23 14:22:12 +0100

    Bonus Notes
    These are notes of various things I faced throughout this project. This will eventually go in the documentation, but it's going to stay here for now. It lists the challenges I faced, the things I learned, as well as the things I wasn't able to do despite my efforts. This project taught me quite a bit, so I think it's good to list them all for the sake of posterity.

--- AdministrationProgramNathan/Program.cs
+++ AdministrationProgramNathan/Program.cs
@@ -15,5 +15,29 @@ namespace AdministrationProgramNathan
             AdminProgram.StartProgram();
 
         }
+
+
+        //Bonus Notes:
+        //New things I learned during this project
+        //  1. The existence of \n for structure in the console.
+        //  2. The proper way a public static modifier can be used.
+        //  3. The instances where a function should be private and how to incorperate that into the rest of the class.
+        //  4. Continue statements, and how to use them.
+        //  5. Alternative uses for the "return" statement within void functions.
+        //  6. 
+
+        //Things that were challenging during this project
+        //  1. Researching and finding the reason why static modifiers would be used.
+        //  2. Initially, thinking of what functions should be private and public, and why they should be public.
+        //      The challenge comes from public functions requiring me to think ahead as to what could possibly want to use them.
+        //  3. 
+
+
+
+        //Things I was unable to do during this project
+        //  1. For the name validity verification, I was unable to make it so that I could detect special characters like @ or ? and !.
+        //      I was hoping to be able to have the program detect all special characters without including the spacebar,
+        //      but I couldn't find any way of doing that. So, in the end I had to settle with just excluding numbers.
+        //  2. 
     }
 }
